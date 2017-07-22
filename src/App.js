4f46d95ed401c749a5738802c533c42b2843dd00
Ricardo vargas <richar.vargas@roket.mx>
2017-07-22 12:40:18 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+  const elementDos = (
+    <div>
+      <h1>Hello, world!dos </h1>
+    </div>
+  );
+    return (
+      <div>
+        {elementDos}
+        {element}
+      </div>
+    );
+  }
+}
+
+export default App;
